2fbfbe3edadbe0477d96ae490c05101088a5d37c
Sonal Gupta <sonalaggarwal555@gmail.com>
2022-06-17 22:19:50 -0700

    fixed build errors

--- src/App.js
+++ src/App.js
@@ -23,8 +23,6 @@ firebase.initializeApp({
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-const storageRef = firebase.storage().ref();
 
 function App() {
 
@@ -111,7 +109,7 @@ function ChatMessages(props) {
 
   return (
     <div className={`message ${messageClass}`}>
-      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'}/>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} alt="profilepic"/>
       <p>{text}</p>
     </div>
   )  
